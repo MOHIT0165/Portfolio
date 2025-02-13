cf74a754fe1c3179e7fe62485b09dc671ecca109
MOHIT0165 <mohitbhardwaj852006@gmail.com>
2025-02-14 00:43:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// document.getElementById("next-tag").addEventListener("click" ,() => {
+//     alert("button is pressed");
+// })
+
+document.getElementById("next-tag1").addEventListener("click" ,() => {
+    prompt("How's whole web is : ");
+});
